78c17669dfa36bd404da0eae90cf6d2fa9141af5
Mohanprasadd24 <mohanprasadd24@gmail.com>
2026-02-23 16:34:36 +0530

    (alpha >=0 && alpha <=127)-ascii value range for smaller and capital letters also char letter=sc.next().charAt(0);

---
+++ src/ascii_input1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ascii_input1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char letter=sc.next().charAt(0);
+        System.out.print((int)letter);
+    }
+}
+
+//public class Solution {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc =new Scanner(System.in);
+//        char alpha =sc.next().charAt(0);
+//        if(alpha >=0 && alpha <=127)
+//        {
+//            int cham =(int)alpha;
+//            System.out.println(cham);
+//        }
+//
+//    }
+//}
