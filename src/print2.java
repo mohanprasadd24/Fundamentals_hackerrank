8a56c5e2743111838625b2b4a97fbc9bf93b77c7
Mohanprasadd24 <mohanprasadd24@gmail.com>
2026-02-23 16:15:33 +0530

    commit init

---
+++ src/print2.java
@@ -0,0 +1,5 @@
+public class print2 {
+    public static void main(String[] args) {
+        System.out.print("Hello World\nHello World");
+    }
+}
---
+++ src/print3.java
@@ -0,0 +1,5 @@
+public class print3 {
+    public static void main(String[] args) {
+        System.out.print("Hello World\tHello World");
+    }
+}
