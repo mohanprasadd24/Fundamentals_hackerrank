ac159a6e3e2d12352097ff1d3b73948db74254a3
Mohanprasadd24 <mohanprasadd24@gmail.com>
2026-02-23 16:21:52 +0530

    for two decimal palces-Math.floor(cgpa*100)/100); to get char input-char grade=sc.next().charAt(0);

---
+++ src/input1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class input1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        int age=sc.nextInt();
+        float cgpa=sc.nextFloat();
+        char grade=sc.next().charAt(0);
+        System.out.println("name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("CGPA: "+Math.floor(cgpa*100)/100);
+        System.out.println("Grade: "+grade);
+    }
+}
